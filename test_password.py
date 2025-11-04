7df528300ba33075634ef9793b59448c79e45961
djouhaina-chelbi <djuhaynach2023@gmail.com>
2025-11-04 20:24:47 +0100

    COD_6

--- test_password.py
+++ test_password.py
@@ -1,5 +1,4 @@
-def test_password_special_character():
-    result = password("Abcd1234")
-    assert result["is_valid"] == False
-    assert "Password must contain at least one special character" in result["errors"]
-
+def test_password_valid():
+    result = password("Abcd@123")
+    assert result["is_valid"] == True
+    assert result["errors"] == []
