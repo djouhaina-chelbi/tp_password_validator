003e837e4101cbeb6875b93720d438333da3466e
djouhaina-chelbi <djuhaynach2023@gmail.com>
2025-11-04 19:58:35 +0100

    COD_3

--- password.py
+++ password.py
@@ -2,6 +2,9 @@ def password(password: str):
     errors = []
     if len(password) < 8:
         errors.append("Password must be at least 8 characters")
+    numbers = re.findall(r"[0-9]", password)
+    if len(numbers) < 2:
+        errors.append("The password must contain at least 2 numbers")
     return {
         "is_valid": len(errors) == 0,
         "errors": errors
--- test_password.py
+++ test_password.py
@@ -1,4 +1,5 @@
-def test_password_too_short():
-    result = password("abc")
+def test_password_two_numbers():
+    result = password("Abcdefgh")
     assert result["is_valid"] == False
-    assert "Password must be at least 8 characters" in result["errors"]
+    assert "The password must contain at least 2 numbers" in result["errors"]
+
